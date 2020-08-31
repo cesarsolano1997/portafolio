82dd6139ffc82acfaf664bbb6aa27074234c02c3
cesarsolano1997 <cesarsolanorobles@gmail.com>
2020-08-31 11:42:28 -0500

    Updates

---
+++ precache-manifest.bc97683e04440b4cc232631a4fd1162c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316a3c47b31282ea27039fb1cedfdbbc",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "2c03bd6175202ce4566d",
+    "url": "/portafolio/static/js/2.12c1f2d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.12c1f2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ead295e428f928a6c2",
+    "url": "/portafolio/static/js/main.6d958b14.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  }
+]);
